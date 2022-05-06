513a9777cf6912b483072e7285d199e5d3772fb5
Kai Heng <johnloh1998@gmail.com>
2022-05-06 22:05:09 +0800

    initialised home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import { Carousel } from 'antd'
+
+import styles from './homeStyle.module.css'
+import FeaturedBrands from './FeaturedBrands'
+
 export default function Home(){
     return (
-        <>
-            This is the home page
-        </>
+            <>
+                <div className={styles.carouselContainer}>
+                    <h2>Carousel</h2>
+                </div>
+                <FeaturedBrands />
+            </>
     )
 }
