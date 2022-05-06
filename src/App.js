2bf92d80f70267dea05066666814694d3f972800
Kai Heng <johnloh1998@gmail.com>
2022-05-06 19:36:19 +0800

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-//import React from 'react'
-
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Navbar from "./Components/Navbar";
+
+import Home from './Components/Home'
+import Product from './Components/Product/Product'
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/product' element={<Product />}></Route>
+      </Routes>
     </>
   );
 }
