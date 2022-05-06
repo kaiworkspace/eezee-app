ab36c82ea5ee0c7e698c7bf809c6db761fac5c84
Kai Heng <johnloh1998@gmail.com>
2022-05-07 00:16:05 +0800

    restored app.css

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import Navbar from "./Components/Navbar";
 import Home from './Components/Home'
 import Product from './Components/Product/Product'
 
+import "./App.css"
 export default function App() {
+  
   return (
-    <>
+    <div>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/product' element={<Product />}></Route>
       </Routes>
-    </>
+    </div>
   );
 }
 
