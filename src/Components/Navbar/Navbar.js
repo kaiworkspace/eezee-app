924b9a2eacbd5bdf9b6f89f3a450ed1edaaab84a
Kai Heng <johnloh1998@gmail.com>
2022-05-06 17:44:34 +0800

    updated styling for nav bar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -11,30 +11,39 @@ export default function Navbar(){
     return (
         <>
             <div className={styles.topBarContainer}>
-                <div className={styles.topBarElement}>
-                    <img
-                        src="https://flagcdn.com/h20/sg.png"
-                        srcset="https://flagcdn.com/h40/sg.png 2x,
-                            https://flagcdn.com/h60/sg.png 3x"
-                        height="20"
-                        alt="Singapore"/>
-                    <p>{info.country}</p>
-                </div>
-                <div className={styles.topBarElement}>
-                    <PhoneOutlined />
-                    <p>{info.contact}</p>
+                <div className={styles.topBarInfoContainer}>
+                    <div className={styles.topBarElement}>
+                        <img
+                            src="https://flagcdn.com/h20/sg.png"
+                            srcset="https://flagcdn.com/h40/sg.png 2x,
+                                https://flagcdn.com/h60/sg.png 3x"
+                            height="20"
+                            alt="Singapore"/>
+                        <p className={styles.topBarText}>{info.country}</p>
+                    </div>
+                    <div className={styles.topBarElement}>
+                        <PhoneOutlined />
+                        <p className={styles.topBarText}>{info.contact}</p>
+                    </div>
+
                 </div>
             </div>
             <div className={styles.mainContainer}>
-                <div>
-                    <img src='https://api.eezee.sg/image/resize?height=44&width=150&url=https://storage.googleapis.com/imgez/eezee-logos/word-whitebg.png'/>
-                    <p>View All brands</p>
-                </div>
-                <div className={styles.searchBarContainer}>
+                <div className={styles.mainLeftContainer}>
+                    <img 
+                        className={styles.logo}
+                        to="/"
+                        src='https://api.eezee.sg/image/resize?height=44&width=150&url=https://storage.googleapis.com/imgez/eezee-logos/word-whitebg.png'/>
                     <SearchBar />
                 </div>
+                
                 <div>
                     Cart
+                </div>
+            </div>
+            <div className={styles.bottomContainer}>
+                <div className={styles.bottomText}>
+                    <p>View All Brands</p>
                 </div>
             </div>
         </>
