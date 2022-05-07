141b90dc43faf2b53c7539577c92e732d643e99b
Kai Heng <johnloh1998@gmail.com>
2022-05-08 02:46:37 +0800

    linked homepage to product page

--- src/Components/Home/MostPopularProducts/MostPopularProducts.js
+++ src/Components/Home/MostPopularProducts/MostPopularProducts.js
@@ -6,19 +6,21 @@ export default function MostPopularProducts(){
     
     const renderProducts=product.map((p)=>{
         return (
-            <div
-                key={p.productId}
-                className={styles.productCard}
-                onClick={()=>{console.log("Product clicked")}}>
-                    <img
-                        className={styles.image} 
-                        src={p.productImg}></img>
-                    <div className={styles.priceContainer}>
-                        <p className={styles.currency}>S$</p>
-                        <h3>{p.productPrice}</h3>
-                    </div>
-                    <p className={styles.productName}>{p.productName}</p>
-            </div>
+            <a href="/product" className={styles.href}>
+                <div
+                    key={p.productId}
+                    className={styles.productCard}
+                    onClick={()=>{console.log("Product clicked")}}>
+                        <img
+                            className={styles.image} 
+                            src={p.productImg}></img>
+                        <div className={styles.priceContainer}>
+                            <p className={styles.currency}>S$</p>
+                            <h3>{p.productPrice}</h3>
+                        </div>
+                        <p className={styles.productName}>{p.productName}</p>
+                </div>
+            </a>
         )
     })
     
